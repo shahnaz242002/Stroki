fd95c7d6ecdaa26890cf3ae42d95e453a7fef54e
shahn <shahn@DESKTOP-P54G1G2>
2023-03-15 20:20:55 +0400

    Add project files.

---
+++ firstProject/Program.cs
@@ -0,0 +1,9 @@
+﻿string sum;
+Console.WriteLine("What is the sum of investment?");
+double vklad = Convert.ToInt32(Console.ReadLine());
+double aksia1 = vklad * 8 / 100;
+double aksia2 = vklad * 95 / 1000;
+double aksia3 = vklad * 10 / 100;
+Console.WriteLine(aksia1);
+Console.WriteLine(aksia2);
+Console.WriteLine(aksia3);
