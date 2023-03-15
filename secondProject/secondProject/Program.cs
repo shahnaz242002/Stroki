09ebfef6e2500c7d185a239f7b7cc5174832ea5c
shahnaz242002 <126859855+shahnaz242002@users.noreply.github.com>
2023-03-15 20:26:37 +0400

    commit

---
+++ secondProject/secondProject/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("What is lenghth of path in km? ");
+int length = Convert.ToInt32(Console.ReadLine());
+int price = 150;
+int otherPrice = 70;
+int result = price + otherPrice * length;
+Console.Write(result);
