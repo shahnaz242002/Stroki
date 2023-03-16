904c0b1393887fc1d38601d2129461ccd9f2e796
shahnaz242002 <shahnazavazova@gmail.com>
2023-03-16 16:48:36 +0400

    сделала поправки в решении

--- firstProject/Program.cs
+++ firstProject/Program.cs
@@ -1,9 +1,8 @@
 ﻿Console.WriteLine("What is the sum of investment?");
 double vklad = Convert.ToInt32(Console.ReadLine());
-double aksia1 = vklad * 8 / 100;
-double aksia2 = vklad * 95 / 1000;
-double aksia3 = vklad * 10 / 100;
+double aksia1 = vklad * 0.06;
+double aksia2 = vklad * 0.095;
+double aksia3 = vklad * 0.15;
 Console.WriteLine(aksia1);
 Console.WriteLine(aksia2);
-Console.WriteLine(aksia3);
 Console.WriteLine(aksia3);
